56b0a93c4e2e8eec5a784d51af0d72c18ca959a9
hiroki minagawa <hiroki6805@gmail.com>
2018-12-28 21:30:44 +0900

    右クリックで旗を立てるように実装

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   <div id="app">
     <button @click="startGame">Start Game</button>
     <table class="minesweeper">
-      <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
-        <Tile v-for="(col, colIndex) in cols" :className="classNames[rowIndex][colIndex]" 
-              :rowIndex="rowIndex" :colIndex="colIndex" :key="colIndex"
-              @clickedRight="setFlag"
-              ></Tile>
+      <tr v-for="(row, rowIndex) in classNames" :key="rowIndex">
+        <Tile
+          v-for="(col, colIndex) in row"
+          :className="classNames[rowIndex][colIndex]"
+          :rowIndex="rowIndex"
+          :colIndex="colIndex"
+          :key="colIndex"
+          @clickedRight="setFlag"
+          @clickedLeft="openTile"
+        ></Tile>
       </tr>
     </table>
   </div>
@@ -19,35 +24,96 @@ export default {
   name: 'App',
   data: () => {
     return {
-      rows: 0,
-      cols: 0,
       classNames: [],
+      mineMap: [],
     };
   },
   components: {
-    Tile: Tile,
+    Tile: Tile
   },
   methods: {
     startGame: function() {
-      this.rows = 10;
-      this.cols = 19;
       this.initializeClassNames();
     },
     initializeClassNames: function() {
-      for (let i = 0; i < this.rows; i += 1) {
-        this.classNames[i] = [];
-        const classNamesRow = this.classNames[i];
-        for (let t = 0; t < this.cols; t += 1) {
+      const MAX_ROW = 10;
+      const MAX_COL = 19;
+      const classNames = [];
+      const mineMap = [];
+      for (let i = 0; i < MAX_ROW; i += 1) {
+        classNames[i] = [];
+        mineMap[i] = [];
+        const classNamesRow = classNames[i];
+        const mineMapRow = mineMap[i];
+        for (let t = 0; t < MAX_COL; t += 1) {
           classNamesRow[t] = 'unopened';
+          mineMapRow[t] = this.setMine();
         }
       }
+      this.classNames =classNames;
+      this.mineMap = mineMap;
     },
-    openTile: function() {
-      
+    setMine: function() {
+      return Math.random() * 6 > 5;
     },
-    setFlag: function(rowIndex, colIndex) {
-      this.classNames[rowIndex].splice(colIndex, 1, 'flagged');
-      console.log(this.classNames);
+    openTile: function(tile) {
+      //if  タイルが地雷なら(isMine())
+      //  押されたタイルのクラスネームを"mine"に変更
+      //  showAll():他のタイル全てに周りの地雷の数に応じたクラスネームを（地雷の場合は地雷を表示）
+      //else タイルが地雷じゃない
+      //  const mineCount = surroundedMineCount() count mine surrounded
+      //  if 周辺に地雷がある時
+      //     changeClassNameBasedMineCount() change ClassName Based on MineCount
+      //   else 
+      //     このタイルのclassNameを"opened"に
+      //     再帰処理部分(if the neighbor has not been opened yet)
+      //        (open the neighbor)
+    },
+    /**
+     * isMine or not
+     * @function
+     * @param {object} tile
+     * @return {boolean}
+     */
+    isMine: function(tile) {
+      // this.mineMapでこのtileが地雷かどうか調べる
+    },
+    /**
+     * show all tile
+     * @function
+     * @return {undefined}
+     */
+    showAll: function() {
+      //for 行
+      //  for 列
+      //    if 地雷なら
+      //      クラスネームを"mine"に変更
+      //    else
+      //      周りの地雷の個数に応じてクラスネームを変更 surroundedMineCount() , changeClassNameBasedMineCount()
+    },
+    /**
+     * count mine surrounded
+     * @function
+     * @param {Object} tile
+     * @return {number}
+     */
+    surroundedMineCount: function(tile){
+      // this.mineMapを使って周辺の地雷の数を調べる
+      // return mineCount;
+    },
+    /**
+     * change ClassName Based on MineCount
+     * @function
+     * @param {number} row
+     * @param {number} col
+     * @param {number} mineCount
+     * @return {undefined}
+     */
+    changeClassNameBasedMineCount: function(row, col, mineCount) {
+      // 地雷の数に応じてクラスネームを変更
+    },
+    setFlag: function(tile) {
+      this.classNames[tile.rowIndex].splice(tile.colIndex, 1, 'flagged');
     },
   },
 };
--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,73 +1,75 @@
 <template>
-  <td :class="className" @click.right.prevent="clicked"></td>
+  <td 
+    :class="className"
+    @click.right.prevent="clickedRight"
+    @click.left.prevent="clickedLeft"
+  ></td>
 </template>
 
 <script>
 export default {
   name: 'Tile',
   props: ['className', 'rowIndex', 'colIndex'],
-  data: function() {
-    return {
-      mined: Math.random() * 6 > 5,
-    };
-  },
   methods: {
-    clicked: function() {
-      this.$emit('clickedRight', this.rowIndex, this.colIndex);
-    }
+    clickedRight: function() {
+      this.$emit('clickedRight', this);
+    },
+    clickedLeft: function() {
+      this.$emit('clickedLeft', this);
+    },
   }
 };
 </script>
 
 <style>
 td.unopened {
-  background: url('../assets/unopened.svg');
+  background: url("../assets/unopened.svg");
   cursor: pointer;
 }
 
 td.opened {
-  background: url('../assets/opened.svg');
+  background: url("../assets/opened.svg");
 }
 
 td.flagged {
-  background: url('../assets/flag.svg');
+  background: url("../assets/flag.svg");
   cursor: pointer;
 }
 
 td.mine {
-  background: url('../assets/mine.png');
+  background: url("../assets/mine.png");
 }
 
 td.mine-neighbor-1 {
-  background: url('../assets/1.svg');
+  background: url("../assets/1.svg");
 }
 
 td.mine-neighbor-2 {
-  background: url('../assets/2.svg');
+  background: url("../assets/2.svg");
 }
 
 td.mine-neighbor-3 {
-  background: url('../assets/3.svg');
+  background: url("../assets/3.svg");
 }
 
 td.mine-neighbor-4 {
-  background: url('../assets/4.svg');
+  background: url("../assets/4.svg");
 }
 
 td.mine-neighbor-5 {
-  background: url('../assets/5.svg');
+  background: url("../assets/5.svg");
 }
 
 td.mine-neighbor-6 {
-  background: url('../assets/6.svg');
+  background: url("../assets/6.svg");
 }
 
 td.mine-neighbor-7 {
-  background: url('../assets/7.svg');
+  background: url("../assets/7.svg");
 }
 
 td.mine-neighbor-8 {
-  background: url('../assets/8.svg');
+  background: url("../assets/8.svg");
 }
 </style>
 
